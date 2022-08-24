f4a9f49034a93b252a1a3026d24be4febc9aa45b
Ceddix <49922771+Ceddix@users.noreply.github.com>
2022-08-25 00:03:56 +0200

    back to main page when pressing ESC (concept)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ function copyToClipboard(copyText, successMessage) {
     navigator.clipboard.writeText(copyText);
     infobox('success', successMessage)
 }
+
+/* back to main page when pressing ESC */
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 27) {
+        location.href = '#'
+    }
+})
