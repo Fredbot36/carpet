dd53d2cd156399e5fac3f59b8ff3e296a1b89b3e
Ceddix <49922771+Ceddix@users.noreply.github.com>
2023-11-06 01:21:39 +0100

    fix: images still not showing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'carpet.craft-together-mc.de',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
