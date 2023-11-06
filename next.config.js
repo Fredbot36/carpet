5196896391a45f84823a26abfc6b74946e76a392
Ceddix <49922771+Ceddix@users.noreply.github.com>
2023-11-06 01:23:33 +0100

    fix: forget this one

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'carpet.craft-together-mc.de',
-                port: '',
-                pathname: '**',
-            },
-        ],
-    },
 }
 
 module.exports = nextConfig
