2b67df454ce8ace4c3413f11a92443e07595958f
Ceddix <49922771+Ceddix@users.noreply.github.com>
2023-11-10 00:15:03 +0100

    feat: moved background video to component and added play/pause button

---
+++ app/components/BackgroundVideo.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, {useEffect, useRef, useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPlay} from '@fortawesome/free-solid-svg-icons'
+import {AnimatePresence, motion, useInView} from 'framer-motion';
+
+export const BackgroundVideo = () => {
+
+    const backgroundVideo = useRef(null);
+    const handlePlayVideo = () => {
+        if (backgroundVideo.current.paused)
+            backgroundVideo.current.play();
+        else
+            backgroundVideo.current.pause();
+    }
+
+    const [isHovering, setIsHovering] = useState(false);
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    };
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
+
+    function useIsVisible(ref) {
+        return useInView(ref)
+    }
+
+    return (
+        <>
+
+            <video
+                ref={backgroundVideo}
+                autoPlay muted loop playsInline
+                className={"w-full h-full absolute object-cover z-0 top-0 brightness-50"}
+            >
+                <source src={"videos/background1.webm"} type={"video/webm; codecs=vp9"}/>
+                {/*TODO: multiple video formats*/}
+                {/*TODO: multiple smaller videos, that get randomly chosen when website loads (rerecord videos)*/}
+                {/*TODO: image on mobile devices*/}
+            </video>
+
+            <AnimatePresence>
+                {useIsVisible(backgroundVideo) ? (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                        className={"fixed top-[1.9rem] right-[1.9rem] block z-50"}
+                    >
+                        <div className={"flex flex-row"}>
+                            <button className={"relative text-2xl font-medium text-neutral-200 bg-neutral-600/[.3] mx-1 px-4 py-3 rounded-lg border border-white/[0.1] transition ease-in-out duration-500 focus:outline-0 hover:border-white/[0.2] hover:shadow-lg hover:shadow-[#34B4F4]/[0.3] hover:bg-white/[0.15]"}
+                                onClick={handlePlayVideo}
+                                onMouseOver={handleMouseOver}
+                                onMouseOut={handleMouseOut}
+                            >
+                                <FontAwesomeIcon icon={faPlay} className={"relative text-2xl top-[.0625rem] drop-shadow-lg"}/>
+                                {isHovering && (
+                                    backgroundVideo.current.paused
+                                        ? (<span className={"text-2xl font-medium text-neutral-200"}>&thinsp;&thinsp;Video fortsetzen</span>)
+                                        : (<span className={"text-2xl font-medium text-neutral-200"}>&thinsp;&thinsp;Video pausieren</span>)
+                                )}
+                            </button>
+                        </div>
+                    </motion.div>
+                ) : ""}
+            </AnimatePresence>
+
+
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import {Inter} from 'next/font/google'
 import {NavBar} from "@/app/components/NavBar";
-import React from "react";
+import {BackgroundVideo} from "@/app/components/BackgroundVideo";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -17,22 +17,14 @@ export default function Home() {
             <section id={"hero"} className={"h-[56rem] flex justify-center items-center box-border relative pt-16 px-12 pb-12 text-center"}>
 
                 {/* background video */}
-                <video
-                    autoPlay muted loop playsInline
-                    className={"w-full h-full absolute object-cover z-0 top-0 brightness-50"}
-                >
-                    <source src={"videos/background1.webm"} type={"video/webm; codecs=vp9"}/>
-                    {/*TODO: multiple video formats*/}
-                    {/*TODO: multiple smaller videos, that get randomly chosen when website loads (rerecord videos)*/}
-                    {/*TODO: image on mobile devices*/}
-                </video>
+                <BackgroundVideo/>
 
                  {/* hero content className={"before:content-[''] before:block before:fixed before:inset-0 before:bg-neutral-950/[.7] before:-z-10 before:pointer-events-none z-0"}*/ }
                 <div className={"z-0"}>
 
                     {/* branding */}
                     <Image
-                        src={`images/logo_lettering.png`}
+                        src={`/images/logo_lettering.png`}
                         alt={`CraftTogetherMC Logo`}
                         className={"inline-block object-cover cursor-pointer transition transform duration-500 hover:scale-105 drop-shadow-2xl"}
                         quality={100}
@@ -49,7 +41,48 @@ export default function Home() {
             </section>
 
             <section id={"content"}>
-
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </section>
 
         </>
