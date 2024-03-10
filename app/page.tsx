ee83ebe8a62ad9e7c1b87a70c776b0cc65ec695d
Ceddix // Cedrik <SecicCedrik@gmail.com>
2024-03-10 02:55:13 +0100

    feat: added social media links

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import {Inter} from 'next/font/google'
-import {NavBar} from "@/app/components/NavBar";
 import {BackgroundVideo} from "@/app/components/BackgroundVideo";
 import VideoPlayer from "@/app/components/VideoPlayer";
+import { RiInstagramLine, RiTiktokLine, RiTwitterXLine, RiYoutubeLine } from 'react-icons/ri';
+import { LuFacebook } from "react-icons/lu";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -11,9 +12,6 @@ export default function Home() {
         <>
 
             {/*TODO: add website preloader (the video/image(s) should be loaded, before the website gets displayed)*/}
-
-            <NavBar/>
-
             {/* HERO SECTION */}
             <section id={"hero"} className={"h-[35rem] flex justify-center items-center box-border relative pt-0 px-12 pb-12 text-center"}>
 
@@ -41,15 +39,18 @@ export default function Home() {
 
             </section>
 
-            <section id={"content"} className={`mb-20`}>
+            <section id={"content"}>
 
                 <div className={`flex justify-center items-center w-full relative`}>
                     <div className={`flex flex-col xl:flex-row sm:w-2/3 mx-10 max-xl:space-y-4 xl:space-x-4 items-center p-4 -mt-28 p-y-auto text-md font-normal text-neutral-200/[.9] bg-neutral-900/[.9] rounded-lg border border-white/[0.1]`}>
+
                         <div className={`w-full h-full max-w-[448px] max-h-[252px] md:min-w-[448px] md:min-h-[252px]`}>
                             {/*  TODO: Add skelleton  */}
                             <VideoPlayer url={"https://youtu.be/nY82tthVJm8"}/>
                         </div>
+
                         <div className={`items-center sm:px-4 lg:px-8 xl:px-4`}>
+
                             <p>
                                 <b>CraftTogetherMC</b> ist ein gemütlicher, aufstrebender Minecraft-Server
                                 und richtet sich an Freunde des <b>originalgetreuen (Vanilla) Minecraft</b>.
@@ -64,12 +65,45 @@ export default function Home() {
                                 <button className={`grow rounded-lg border border-2 border-white/[0.1] bg-green-800/[.4] min-h-12 p-1`}>Jetzt Joinen!</button>
                                 <button className={`grow rounded-lg border border-2 border-white/[0.1] bg-cyan-800/[.4] min-h-12 p-1`}>Weitere Informationen</button>
                             </div>
+
                         </div>
+
                     </div>
                 </div>
 
-                <div>
-                {/*  TODO: Insert Social Media Links  */}
+                <div className={`flex flex-col justify-center items-center w-full relative pt-7 text-gray-300`}>
+                    <p>Folge uns auf Social Media:</p>
+                    <ul className={`flex flex-row font-extralight focus:[&_a]:text-white focus:[&_a]:outline-none focus:[&_a]:ring-0 [&_li]:mx-1.5 [&_li]:my-1 [&_li]:p-1 [&_li]:drop-shadow-md hover:[&_li]:text-white transition-all duration-200`}>
+                        <li>
+                            <a target="_blank" href="https://www.instagram.com/crafttogethermc.de/">
+                                <RiInstagramLine size={30}/>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a target="_blank" href="https://www.tiktok.com/@crafttogethermc">
+                                <RiTiktokLine size={30}/>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a target="_blank" href="https://www.youtube.com/c/CraftTogetherDE">
+                                <RiYoutubeLine size={30}/>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a target="_blank" href="https://www.facebook.com/CraftTogetherMC">
+                                <LuFacebook size={30}/>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a target="_blank" href="https://twitter.com/CraftTogetherDE">
+                                <RiTwitterXLine size={30}/>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
 
             </section>
