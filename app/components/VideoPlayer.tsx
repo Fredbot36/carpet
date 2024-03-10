7510dc75e5906bb6146238e1902c9040dd2a162a
Ceddix // Cedrik <SecicCedrik@gmail.com>
2024-03-10 01:44:04 +0100

    feat: switched to "react-player" for yt

---
+++ app/components/VideoPlayer.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+
+export default function VideoPlayer(props: { url: any; controls?: boolean; playing?: boolean; loop?: boolean; muted?: boolean }) {
+    const [isClient, setIsClient] = useState(false)
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    return (
+        isClient ?
+            <div className={'relative pt-[56.25%]'}>
+                <ReactPlayer
+                    url={props.url}
+                    controls={props.controls}
+                    playing={props.playing}
+                    loop={props.loop}
+                    muted={props.muted}
+                    className={'absolute top-0 left-0'}
+                    width={'100%'}
+                    height={'100%'}
+                    // picture in picture
+                    pip={false}
+                />
+            </div> : <p>There was an error loading the video player!</p>
+    )
+};
