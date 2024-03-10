ed795a1469b7f2f9134fe53126e13d1e4bd8aa66
Ceddix // Cedrik <SecicCedrik@gmail.com>
2024-03-10 02:55:04 +0100

    feat: added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Fira_Sans } from 'next/font/google'
+import { NavBar } from '@/app/components/NavBar'
+import Link from 'next/link'
 
 const fira = Fira_Sans({ subsets: ['latin'], weight: "400" })
 
@@ -22,7 +24,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={fira.className}>{children}</body>
+      <body className={fira.className}>
+
+        <NavBar/>
+
+        {children}
+
+        <section id={'footer'} className={`flex flex-col mt-5`}>
+          <div className={`relative h-52 absolute left-0 right-0 bg-gradient-to-b from-transparent to-neutral-950`}></div>
+
+          <div className={`relative flex flex-row justify-between items-end bg-neutral-950 text-sm text-neutral-300 p-10 pt-16`}>
+
+            <div>
+              <p><b><Link href={"/"}>CraftTogetherMC</Link></b> – Freebuild mit Vanilla-Feeling</p>
+              <p className={`text-neutral-400`}>We are not affiliated with or endorsed by Mojang, AB.</p>
+              <small>Created with ♥️ by <a target="_blank" href="https://cedrik.me">Ceddix</a></small>
+            </div>
+
+            <div>
+              <p>
+                <Link href={"/impressum"}>Impressum</Link> | <Link href={"/datenschutz"}>Datenschutz</Link>
+              </p>
+            </div>
+
+          </div>
+        </section>
+
+      </body>
     </html>
   )
 }
