14d8ab8ebdf72709e3ad7d3144f38a51e3e3f493
Ceddix <49922771+Ceddix@users.noreply.github.com>
2024-04-08 01:53:12 +0200

    feat: some improvments to the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,14 +31,14 @@ export default function RootLayout({
         {children}
 
         <section id={'footer'} className={`flex flex-col mt-5`}>
-          <div className={`relative h-52 absolute left-0 right-0 bg-gradient-to-b from-transparent to-neutral-950`}></div>
+          <div className={`relative h-52 left-0 right-0 bg-gradient-to-b from-transparent to-neutral-950`}></div>
 
-          <div className={`relative flex flex-row justify-between items-end bg-neutral-950 text-sm text-neutral-300 p-10 pt-16`}>
+          <div className={`relative flex flex-col sm:flex-row sm:items-end justify-between gap-4 bg-neutral-950 text-sm text-neutral-300 p-10 pt-16`}>
 
             <div>
               <p><b><Link href={"/"}>CraftTogetherMC</Link></b> – Freebuild mit Vanilla-Feeling</p>
               <p className={`text-neutral-400`}>We are not affiliated with or endorsed by <Link target="_blank" href={"https://www.minecraft.net/de-de"}>Mojang, AB</Link>.</p>
-              <small>Created with ♥️ by <Link target="_blank" href="https://cedrik.me">Ceddix</Link></small>
+              <small><Link target="_blank" href="https://cedrik.me">Created with ❤ by Ceddix!</Link></small>
             </div>
 
             <div>
